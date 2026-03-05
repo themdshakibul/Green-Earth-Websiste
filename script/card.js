98baaa37594d19f27fd0cb6911bf48651088ac6e
themdshakibul <mdshakibul88240@gmail.com>
2026-03-05 15:14:54 +0600

    add btn active

--- script/card.js
+++ script/card.js
@@ -1,7 +1,19 @@
 const categoriesContainer = document.getElementById("categoriesContainer");
 const treesContainer = document.getElementById("treesContainer");
+const loadingSpiner = document.getElementById("loadingSpiner");
+const allTreesBtn = document.getElementById("allTreesBtn");
 
-// async function
+function ShowLoading() {
+  loadingSpiner.classList.remove("hidden");
+  loadingSpiner.classList.add("flex");
+  treesContainer.innerHTML = "";
+}
+
+function hiddenLoading() {
+  loadingSpiner.classList.add("hidden");
+}
+
+// Load categories btn
 async function loadCategories() {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/categories",
@@ -11,13 +23,53 @@ async function loadCategories() {
     const btn = document.createElement("button");
     btn.className = "btn btn-outline w-full text-xl text-base-content";
     btn.textContent = categories.category_name;
+    btn.onclick = () => selectCategory(categories.id, btn);
     categoriesContainer.appendChild(btn);
   });
 }
 
+async function selectCategory(categoriesId, btn) {
+  console.log(categoriesId, btn);
+  ShowLoading();
+  const allbuttons = document.querySelectorAll(
+    "#categoriesContainer button, #treesContainer",
+  );
+  allbuttons.forEach((btn) => {
+    btn.classList.remove("btn-success");
+    btn.classList.add("btn-outline");
+  });
+  btn.classList.add("btn-success");
+  btn.classList.remove("btn-outline");
+
+  fetch(`https://openapi.programming-hero.com/api/category/${categoriesId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      displayTrees(data.plants);
+    });
+  hiddenLoading();
+}
+
+allTreesBtn.addEventListener("click", () => {
+  // update active btn style
+  const allbuttons = document.querySelectorAll(
+    "#categoriesContainer button, #allTreesBtn",
+  );
+  allbuttons.forEach((btn) => {
+    btn.classList.remove("btn-success");
+    btn.classList.add("btn-outline");
+  });
+  allTreesBtn.classList.add("btn-success");
+  allTreesBtn.classList.remove("btn-outline");
+
+  loadTrees();
+});
+
 async function loadTrees() {
+  ShowLoading();
   const res = await fetch("https://openapi.programming-hero.com/api/plants");
   const data = await res.json();
+  hiddenLoading();
   displayTrees(data.plants);
 }
 
@@ -28,24 +80,25 @@ function displayTrees(trees) {
     card.innerHTML = `
         <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
+            src="${tree.image}"
+            alt="${tree.name}"
+            title="${tree.name}"
+            class="h-40 w-full object-cover"
           />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Card Title</h2>
+          <h2 class="card-title">${tree.name}</h2>
           <p class="line-clamp-2">
-            A card component has a figure, a body part, and inside body
-            there are title and actions parts
+            ${tree.description}
           </p>
           <div
             class="badge badge-outline rounded-full text-success px-4"
           >
-            NEW
+            ${tree.category}
           </div>
           <div class="card-actions items-center flex justify-between">
-            <h2 class="font-bold text-xl text-success">$5000</h2>
-            <button class="btn btn-primary">Add To Card</button>
+            <h2 class="font-bold text-xl text-success">$${tree.price}</h2>
+            <button class="btn btn-primary">Card</button>
           </div>
         </div>
     `;
